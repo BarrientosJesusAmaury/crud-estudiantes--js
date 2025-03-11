e0fc78410208d70e4a09121e0d746e4fdc71008a
Barrientos Jesus Amaury <Jesusaba1904@gmail.com>
2025-03-11 13:38:57 -0400

    Agregar función de cálculo de promedios en Reportes.js

---
+++ modules/reportes.js
@@ -0,0 +1,11 @@
+import { GestorEstudiantes } from "./GestorEstudiantes.js";
+
+export const Reportes = {
+    promedioPorEstudiante() {
+        return GestorEstudiantes.listarEstudiantes().map(e => {
+            const calificaciones = Object.values(e.calificaciones);
+            const promedio = calificaciones.reduce((a, b) => a + b, 0) / calificaciones.length || 0;
+            return { nombre: e.nombre, promedio, area: e.area };
+        });
+    }
+};
