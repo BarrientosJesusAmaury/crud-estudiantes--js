9832ed706bc6cf5fad37cd2f0c643cf65b32deed
Barrientos Jesus Amaury <Jesusaba1904@gmail.com>
2025-03-11 13:49:34 -0400

    Implementar cálculo de promedios por materia en Reportes.js

--- modules/reportes.js
+++ modules/reportes.js
@@ -11,5 +11,24 @@ export const Reportes = {
 
     buscarEstudiante(criterio) {
         return GestorEstudiantes.buscarEstudiante(criterio);
+    },
+
+    promedioPorMateria() {
+        let materias = { Matematicas: 0, Programacion: 0, Ciencias: 0, Lengua: 0 };
+        let totalEstudiantes = GestorEstudiantes.listarEstudiantes().length;
+
+        GestorEstudiantes.listarEstudiantes().forEach(e => {
+            materias.Matematicas += e.calificaciones.Matematicas;
+            materias.Programacion += e.calificaciones.Programacion;
+            materias.Ciencias += e.calificaciones.Ciencias;
+            materias.Lengua += e.calificaciones.Lengua;
+        });
+
+        return {
+            Matematicas: (materias.Matematicas / totalEstudiantes) || 0,
+            Programacion: (materias.Programacion / totalEstudiantes) || 0,
+            Ciencias: (materias.Ciencias / totalEstudiantes) || 0,
+            Lengua: (materias.Lengua / totalEstudiantes) || 0
+        };
     }
 };
