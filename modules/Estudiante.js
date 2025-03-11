0eea4a4ea1ea72dec732b40bce755b727769cee5
Barrientos Jesus Amaury <Jesusaba1904@gmail.com>
2025-03-11 13:16:23 -0400

    Añadir 4 materias básicas (Matemáticas, Programación, Ciencias y Lengua) en Estudiante.js

--- modules/Estudiante.js
+++ modules/Estudiante.js
@@ -1,11 +1,18 @@
 export class Estudiante {
     static ultimoId = 0;
 
-    constructor(nombre, edad, nivel) {
-        Estudiante.ultimoId++; 
+    constructor(nombre, edad, nivel, area, calificaciones) {
+        Estudiante.ultimoId++;
         this.id = Estudiante.ultimoId;
         this.nombre = nombre;
         this.edad = edad;
         this.nivel = nivel;
+        this.area = area;
+        this.calificaciones = calificaciones || {
+            Matematicas: 0,
+            Programacion: 0,
+            Ciencias: 0,
+            Lengua: 0
+        };
     }
 }
