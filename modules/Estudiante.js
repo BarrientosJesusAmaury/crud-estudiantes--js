e5043642ee25efe482f59494b7b9b07e76d7cf22
Barrientos Jesus Amaury <Jesusaba1904@gmail.com>
2025-03-11 10:48:55 -0400

    Añadir clase Estudiante

---
+++ modules/Estudiante.js
@@ -0,0 +1,11 @@
+export class Estudiante {
+    static ultimoId = 0;
+
+    constructor(nombre, edad, nivel) {
+        Estudiante.ultimoId++; 
+        this.id = Estudiante.ultimoId;
+        this.nombre = nombre;
+        this.edad = edad;
+        this.nivel = nivel;
+    }
+}
